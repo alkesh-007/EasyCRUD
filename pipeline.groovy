9597000ae3114d219c37d6f1cd1a7ca0d7ec8d06
obidea777 <backup@onebillionideas.io>
2026-08-20 21:57:29 +0530

    changes done

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,7 +10,7 @@ pipeline {
         stage('build') {
             steps {
                 sh '''cd backend
-                mvn clean package'''
+                mvn clean package -DskipTests'''
             }
         }
         stage('Deploy') {
